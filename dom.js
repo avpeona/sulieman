58ea2fc81fcaaeb949168e2f182ecc95e010ee43
avpeona <100135398+avpeona@users.noreply.github.com>
2022-02-21 15:11:07 +0100

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,41 @@
+function request(url, cb) {
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        cb(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  
+  document.getElementById("submit").addEventListener("click", ()=> {
+    
+   if(document.querySelector('#word').value == ""){
+      alert("there's no word to translate!");
+    }
+
+    
+        
+    const div = document.querySelector(".container");
+    div.textContent = "";
+    let word = document.getElementById("word").value;
+    
+      request(
+        "http://api.giphy.com/v1/gifs/search?q=" +
+        word +
+          "&api_key=w5iGda16vtNszVULYss6thQOeeaNpdK6",
+        data => {
+            data.data.forEach(element => {
+            const img = document.createElement("img");
+            img.setAttribute("class", "image");
+            img.setAttribute("src","https://media.giphy.com/media/"+element.id+"/giphy.gif");
+            div.appendChild(img);
+            
+  });
+        }
+      );
+    
+  });
